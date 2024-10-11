c7174412d1cfe2318236dc3f2e1e06f57fa5f403
morimc <mori.mcatherine@gmail.com>
2024-10-11 15:41:18 +0900

    style: updated file names and folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import Home from "./(pages)/home/Home";
+"use client";
 
-export default function page() {
+import { useRef } from "react";
+import Hero from "@/components/sections/Hero";
+import NavBar from "@/components/sections/NavBar";
+import ShopPrev from "@/components/sections/ShopPrev";
+import Footer from "@/components/sections/Footer";
+
+const Home = () => {
+  const heroRef = useRef<HTMLElement>(null);
+
   return (
-    <>
-      <Home />
-    </>
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <NavBar heroRef={heroRef} />
+      <Hero ref={heroRef} />
+      <ShopPrev />
+      <Footer />
+    </div>
   );
-}
+};
+
+export default Home;
